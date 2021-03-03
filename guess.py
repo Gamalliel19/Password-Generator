4b0390fa2f83a3da9adda203f9463a355a2c305e
Gamalliel Sharon <you@example.com>
2021-03-03 08:58:06 +0700

    adding guess the number project

--- guess.py
+++ guess.py
@@ -1,3 +1,12 @@
 nilai = 80
-
 tebak = int(input("Tebak angka dari 1 sampai 100 : "))
+
+while (nilai != tebak):
+    if tebak < nilai:
+        print('TEBAKAN KAMU TERLALU RENDAH, AYO TEBAK LAGI.')
+        tebak = int(input("Tebak angka dari 1 sampai 100 : "))
+    if tebak > nilai:
+        print('TEBAKAN KAMU TERLALU TINGGI, AYO TEBAK LAGI.')
+        tebak = int(input("Tebak angka dari 1 sampai 100 : "))
+
+print("KAMU BERHASIL MENEBAK ANGKANYA.")
